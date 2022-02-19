0a5f80b9873576cf1f65e18acc81e1563605d594
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-02-19 17:57:37 +0100

    Enable Err to have any value

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,22 @@ export type Ok<T> = {
   value: T
 }
 
-export type Err = {
+export type Err<T> = {
   type: ResultType.Err
-  message: string
+  error: T
 }
 
-export type Result<T> = Ok<T> | Err
+export type Result<Value, Error> = Ok<Value> | Err<Error>
 
 enum ResultType {
   Ok = 'Ok',
   Err = 'Err',
 }
 
-export const err = (message: string): Err => {
+export const err = <Error>(error: Error): Err<Error> => {
   return {
     type: ResultType.Err,
-    message,
+    error,
   }
 }
 
@@ -29,39 +29,39 @@ export const ok = <T>(value: T): Ok<T> => {
   }
 }
 
-const _match = <A, B>(
-  result: Result<A>,
-  onOk: (value: A) => Result<B>,
-  onError: (message: string) => Result<A>,
-): Result<A> | Result<B> => {
+const _match = <A, B, Error>(
+  result: Result<A, Error>,
+  onOk: (value: A) => Result<B, Error>,
+  onError: (error: Error) => Result<A, Error>,
+): Result<A, Error> | Result<B, Error> => {
   switch (result.type) {
     case ResultType.Ok:
       return onOk(result.value)
     case ResultType.Err:
-      return onError(result.message)
+      return onError(result.error)
   }
 }
 
-export const match = <A, B>(
-  result: Result<A>,
+export const match = <A, B, Error>(
+  result: Result<A, Error>,
   onOk: (value: A) => B | void,
-  onError: (message: string) => B | void,
+  onError: (error: Error) => B | void,
 ): B | void => {
   switch (result.type) {
     case ResultType.Ok:
       return onOk(result.value)
     case ResultType.Err:
-      return onError(result.message)
+      return onError(result.error)
   }
 }
 
 /**
  * Apply a function to a result. If the result is Ok, it will be converted. If the result is an Err, the same error value will propagate through.
  */
-export const map = <A, B>(
+export const map = <A, B, Error>(
   fn: (value: A) => B,
-  result: Result<A>,
-): Result<B> | Result<A> => {
+  result: Result<A, Error>,
+): Result<B, Error> | Result<A, Error> => {
   return _match(
     result,
     (value) => ok(fn(value)),
@@ -72,10 +72,10 @@ export const map = <A, B>(
 /**
  * Apply a function if both results are Ok. If not, the first Err will propagate through.
  */
-export const map2 = <A, B, Value>(
+export const map2 = <A, B, Value, Error>(
   fn: (valueA: A, valueB: B) => Value,
-  resultA: Result<A>,
-  resultB: Result<B>,
+  resultA: Result<A, Error>,
+  resultB: Result<B, Error>,
 ): any => {
   return _match(
     resultA,
@@ -96,10 +96,10 @@ export const map2 = <A, B, Value>(
  *
  * This is for obvious reasons not included in the original Elm API
  */
-export const tap = <A>(
+export const tap = <A, Error>(
   fn: (value: A) => void,
-  result: Result<A>,
-): Result<A> => {
+  result: Result<A, Error>,
+): Result<A, Error> => {
   return _match(
     result,
     (value) => {
@@ -113,10 +113,10 @@ export const tap = <A>(
 /**
  * Chain together a sequence of computations that may fail
  */
-export const andThen = <A, B>(
-  fn: (value: A) => Result<B>,
-  result: Result<A>,
-): Result<A> | Result<B> => {
+export const andThen = <A, B, Error>(
+  fn: (value: A) => Result<B, Error>,
+  result: Result<A, Error>,
+): Result<A, Error> | Result<B, Error> => {
   return _match(
     result,
     (value) => fn(value),
@@ -127,21 +127,25 @@ export const andThen = <A, B>(
 /**
  * Transform an Err value
  */
-export const mapError = <A>(
-  fn: (message: string) => string,
-  result: Result<A>,
-): Result<A> => {
+export const mapError = <A, X, Y>(
+  fn: (error: X) => Y,
+  result: Result<A, X>,
+): Result<A, Y> | Result<A, X> => {
   return _match(
     result,
     () => result,
-    (message) => err(fn(message)),
+    // @ts-expect-error Will fix later
+    (error) => err(fn(error)),
   )
 }
 
 /**
  * If the result is Ok return the value, but if the result is an Err then return a given default value.
  */
-export const withDefault = <A>(defaultValue: A, result: Result<A>): A => {
+export const withDefault = <A, Error>(
+  defaultValue: A,
+  result: Result<A, Error>,
+): A => {
   return match(
     result,
     (value) => value,
