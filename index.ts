50b11e9471e8d9ef604fac19048dcec5e8189c99
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-02-19 12:45:14 +0100

    Cleanup

--- index.ts
+++ index.ts
@@ -1,47 +1,46 @@
 export type Ok<T> = {
-  type: OkType
-  value: T
-}
+  type: ResultType.OkType;
+  value: T;
+};
 
 export type Error = {
-  type: ErrorType
-  message: string
+  type: ResultType.ErrorType;
+  message: string;
+};
+
+export type Result<T> = Ok<T> | Error;
+
+enum ResultType {
+  OkType = "Ok",
+  ErrorType = "Error",
 }
 
-export type Result<T> = Ok<T> | Error
-  
-type ResultType = OkType | ErrorType
-  
-type OkType = 'Ok'
-  
-type ErrorType = 'Error'
-  
 export const error = (message: string): Error => {
   return {
-    type: 'Error',
+    type: ResultType.ErrorType,
     message,
-  }
-}
+  };
+};
 
 export const ok = <T>(value: T): Ok<T> => {
   return {
-    type: 'Ok',
+    type: ResultType.OkType,
     value,
-  }
-}
+  };
+};
 
-const match = <T>(
-  result: Result<T>,
-  onOk: (value: T) => Result<T>,
-  onError: (message: string) => Result<T>,
-): Result<T> => {
-  switch (input.type) {
-    case OkType:
-      return onOk(input.value)
-    case ErrorType:
-      return onError(input.message)
+const match = <A, B>(
+  result: Result<A>,
+  onOk: (value: A) => Result<B>,
+  onError: (message: string) => Result<A>
+): Result<A> | Result<B> => {
+  switch (result.type) {
+    case ResultType.OkType:
+      return onOk(result.value);
+    case ResultType.ErrorType:
+      return onError(result.message);
   }
-}
+};
 
 /**
  * Apply a function to a result. If the result is Ok, it will be converted. If the result is an Err, the same error value will propagate through.
@@ -51,41 +50,41 @@ const match = <T>(
  */
 export const map = <A, B>(
   fn: (value: A) => B,
-  result: Result<A>,
-): Result<B> => {
+  result: Result<A>
+): Result<B> | Result<A> => {
   return match(
     result,
     (value) => ok(fn(value)),
-    () => result,
-  )
-}
+    () => result
+  );
+};
 
-export const map2 = <A, B, Value>(
-  fn: (a: A, b: B) => Value,
-  resultA: Result<A>,
-  resultB: Result<B>,
-): Result<Value> => {
-  return match(
-    result,
-    (value) => ok(fn(value)),
-    () => result,
-  )
-}
+// export const map2 = <A, B, Value>(
+//   fn: (a: A, b: B) => Value,
+//   resultA: Result<A>,
+//   resultB: Result<B>,
+// ): Result<Value> => {
+//   return match(
+//     result,
+//     (value) => ok(fn(value)),
+//     () => result,
+//   )
+// }
 
 export const tap = <A>(
   fn: (value: A) => void,
-  result: Result<A>,
+  result: Result<A>
 ): Result<A> => {
   return match(
     result,
     (value) => {
-      fn(value)
-      return result
+      fn(value);
+      return result;
     },
-    () => result,
-  )
-}
-  
+    () => result
+  );
+};
+
 /**
  * Chain together a sequence of computations that may fail
  * @param fn
@@ -94,19 +93,22 @@ export const tap = <A>(
  */
 export const andThen = <A, B>(
   fn: (value: A) => Result<B>,
-  result: Result<A>,
-): Result<B> => {
+  result: Result<A>
+): Result<A> | Result<B> => {
   return match(
     result,
     (value) => fn(value),
-    () => result,
-  )
-}
-  
-export mapError = <A>(fn: (message: string) => string, result: Result<A>): Result<A> => {
+    () => result
+  );
+};
+
+export const mapError = <A>(
+  fn: (message: string) => string,
+  result: Result<A>
+): Result<A> => {
   return match(
-     result,
-     () => result,
-     (message) => error(fn(message))
-  )
-}
+    result,
+    () => result,
+    (message) => error(fn(message))
+  );
+};
